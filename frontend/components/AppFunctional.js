f70de47253104b0e93f97598805e4bce35f3c1c0
Rebecca-Stone <primstone16@gmail.com>
2022-03-11 02:44:01 -0600

    broke coordnate logic up. works alot better and passed more tests

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 export default function AppFunctional(props) {
-  //need slices of state for coordinates(x, y), count, email, submitMessage, directionMessage
-
   const URL = "http://localhost:9000/api/result";
   const [cordX, setCordX] = useState(2);
   const [cordY, setCordY] = useState(2);
@@ -31,7 +29,7 @@ export default function AppFunctional(props) {
     };
     newPlayer(newInfo);
     evt.preventDefault();
-    setEmail('');
+    setEmail("");
   };
 
   const reset = () => {
@@ -41,6 +39,46 @@ export default function AppFunctional(props) {
     setEmail("");
     setSubmitMessage("");
     setDirectionMessage("");
+  };
+
+  const right = () => {
+    if (cordX < 3) {
+      setCordX(cordX + 1);
+      setCount(count + 1);
+      setDirectionMessage("");
+    } else {
+      setDirectionMessage("You can't go right");
+    }
+  };
+
+  const left = () => {
+    if (cordX > 1) {
+      setCordX(cordX - 1);
+      setCount(count + 1);
+      setDirectionMessage("");
+    } else {
+      setDirectionMessage("You can't go left");
+    }
+  };
+
+  const up = () => {
+    if (cordY > 1) {
+      setCordY(cordY - 1);
+      setCount(count + 1);
+      setDirectionMessage("");
+    } else {
+      setDirectionMessage("You can't go up");
+    }
+  };
+
+  const down = () => {
+    if (cordY < 3) {
+      setCordY(cordY + 1);
+      setCount(count + 1);
+      setDirectionMessage("");
+    } else {
+      setDirectionMessage("You can't go down");
+    }
   };
 
   return (
@@ -69,87 +107,32 @@ export default function AppFunctional(props) {
         </h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
+        <button id="left" onClick={left}>
+          LEFT
+        </button>
+        <button id="up" onClick={up}>
+          UP
+        </button>
+        <button id="right" onClick={right}>
+          RIGHT
+        </button>
+        <button id="down" onClick={down}>
+          DOWN
+        </button>
         <button id="reset" onClick={reset}>
           reset
         </button>
       </div>
       <form onSubmit={onSubmit}>
-        <input 
-        id="email" 
-        type="email" 
-        placeholder="type email"
-        onChange={evt => setEmail(evt.target.value)}
-        value={email}
+        <input
+          id="email"
+          type="email"
+          placeholder="type email"
+          onChange={(evt) => setEmail(evt.target.value)}
+          value={email}
         ></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
   );
 }
-
-//I think I need to break this up in to different functions instead of so many else if statements
-
-// onClick = (id) => {
-//     if (id === "left") {
-//       if (cordX > 1) {
-//         this.setState({
-//           ...this.state,
-//           x: this.state.x - 1,
-//           count: this.state.count + 1,
-//         });
-//       } else {
-//         this.setState({
-//           ...this.state,
-//           directionMessage: "You can't go left",
-//         });
-//       }
-//     } 
-    
-//     else if (id === "right") {
-//       if (this.state.x < 3) {
-//         this.setState({
-//           ...this.state,
-//           x: this.state.x + 1,
-//           count: this.state.count + 1,
-//         });
-//       } else {
-//         this.setState({
-//           ...this.state,
-//           directionMessage: "You can't go right",
-//         });
-//       }
-//     } 
-    
-//     else if (id === "up") {
-//       if (this.state.y > 1) {
-//         this.setState({
-//           ...this.state,
-//           x: this.state.y - 1,
-//           count: this.state.count + 1,
-//         });
-//       } else {
-//         this.setState({
-//           ...this.state,
-//           directionMessage: "You can't go up",
-//         });
-//       }
-//     } 
-    
-//     else if (id === "down") {
-//       if (this.state.y < 3) {
-//         this.setState({
-//           ...this.state,
-//           x: this.state.y + 1,
-//           count: this.state.count + 1,
-//         });
-//       } else {
-//         this.setState({
-//           ...this.state,
-//           directionMessage: "You can't go down",
-//         });
-//       }
-//     }
