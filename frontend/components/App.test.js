bdb5731c877bc054bfc72710cf27c2ff8038ad4d
Rebecca-Stone <primstone16@gmail.com>
2022-03-11 05:05:33 -0600

    finished all tasks and tests

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,33 +1,30 @@
-// import React from "react";
-// import { render, fireEvent, screen } from "@testing-library/react";
-// import "@testing-library/jest-dom/extend-expect";
-// import AppClass from "./AppClass";
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import AppClass from "./AppClass";
 
-// // const emailInput = () => screen.getByPlaceholderText("type email");
-// // const submitBtn = () => screen.getByDisplayValue("submit")
-// // Write your tests here
-// beforeEach(() => {
-//   render(<AppClass />);
-// });
+beforeEach(() => {
+  render(<AppClass />);
+});
 
-// afterEach(() => {
-//   window.localStorage.clear();
-// });
+afterEach(() => {
+  window.localStorage.clear();
+});
 
-// describe("AppClass component", () => {
-//   test("sanity", () => {
-//     expect(true).toBe(true);
-//   });
+describe("AppClass component", () => {
+  test("sanity", () => {
+    expect(true).toBe(true);
+  });
 
-//   test("Renders the heading", () => {
-//     const heading = screen.queryAllByTestId("Coordinates", { exact: false });
-//     expect(heading).toBeVisible();
-//     // expect(heading).toBeInTheDocument();
-//   })
+  test("Renders the heading", () => {
+    const heading = screen.queryByText("Coordinates", { exact: false });
+    expect(heading).toBeVisible();
+    expect(heading).toBeInTheDocument();
+  })
 
-//   // test("can enter a new email and it renders to the screen", async () => {
-//   //   fireEvent.change(emailInput(), { target: { value: "abc123@email.com" }})
-//   //   fireEvent.click(submitBtn());
-//   //   await screen.findByText("abc123 win", { exact: false });
-//   // })
-// });
+  test("Renders the counter", () => {
+    const counter = screen.queryByText("You moved 0 times", { exact: false });
+    expect(counter).toBeVisible();
+    expect(counter).toBeInTheDocument();
+  })
+});
