8f040b78903bd7ae7a9021c63fcd8f256dae9d34
Rebecca-Stone <primstone16@gmail.com>
2022-03-11 02:25:16 -0600

    got email to rest on submit in functal component. also rethinking logic for else if statements

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function AppFunctional(props) {
   //need slices of state for coordinates(x, y), count, email, submitMessage, directionMessage
@@ -8,46 +8,47 @@ export default function AppFunctional(props) {
   const [cordX, setCordX] = useState(2);
   const [cordY, setCordY] = useState(2);
   const [count, setCount] = useState(0);
-  const [email, setEmail] = useState('');
-  const [submitMessage, setSubmitMessage] = useState('');
-  const [directionMessage, setDirectionMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [submitMessage, setSubmitMessage] = useState("");
+  const [directionMessage, setDirectionMessage] = useState("");
 
-  const newPlayer = newInfo => {
-    axios.post(URL, newInfo)
-    .then(res => {
-      console.log(res)
-      setSubmitMessage(res.data.message)
-    })
-    .catch(err => 
-      console.log(err)
-      )
-  }
+  const newPlayer = (newInfo) => {
+    axios
+      .post(URL, newInfo)
+      .then((res) => {
+        console.log(res.data);
+        setSubmitMessage(res.data.message);
+      })
+      .catch((err) => console.log(err));
+  };
 
-  const onSubmit = evt => {
+  const onSubmit = (evt) => {
     const newInfo = {
       x: cordX,
       y: cordY,
       steps: count,
-      email: email
-    }
+      email: email,
+    };
     newPlayer(newInfo);
     evt.preventDefault();
     setEmail('');
-  }
+  };
 
   const reset = () => {
     setCordX(2);
     setCordY(2);
     setCount(0);
-    setEmail('');
-    setSubmitMessage('');
-    setDirectionMessage('');
-  }
+    setEmail("");
+    setSubmitMessage("");
+    setDirectionMessage("");
+  };
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates ({cordX}, {cordY})</h3>
+        <h3 id="coordinates">
+          Coordinates ({cordX}, {cordY})
+        </h3>
         <h3 id="steps">You moved {count} times</h3>
       </div>
       <div id="grid">
@@ -65,19 +66,90 @@ export default function AppFunctional(props) {
         <h3 id="message">
           {submitMessage}
           {directionMessage}
-          </h3>
+        </h3>
       </div>
       <div id="keypad">
         <button id="left">LEFT</button>
         <button id="up">UP</button>
         <button id="right">RIGHT</button>
         <button id="down">DOWN</button>
-        <button id="reset" onClick={reset}>reset</button>
+        <button id="reset" onClick={reset}>
+          reset
+        </button>
       </div>
-      <form onSubmit={onSubmit} >
-        <input id="email" type="email" placeholder="type email"></input>
+      <form onSubmit={onSubmit}>
+        <input 
+        id="email" 
+        type="email" 
+        placeholder="type email"
+        onChange={evt => setEmail(evt.target.value)}
+        value={email}
+        ></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
+
+//I think I need to break this up in to different functions instead of so many else if statements
+
+// onClick = (id) => {
+//     if (id === "left") {
+//       if (cordX > 1) {
+//         this.setState({
+//           ...this.state,
+//           x: this.state.x - 1,
+//           count: this.state.count + 1,
+//         });
+//       } else {
+//         this.setState({
+//           ...this.state,
+//           directionMessage: "You can't go left",
+//         });
+//       }
+//     } 
+    
+//     else if (id === "right") {
+//       if (this.state.x < 3) {
+//         this.setState({
+//           ...this.state,
+//           x: this.state.x + 1,
+//           count: this.state.count + 1,
+//         });
+//       } else {
+//         this.setState({
+//           ...this.state,
+//           directionMessage: "You can't go right",
+//         });
+//       }
+//     } 
+    
+//     else if (id === "up") {
+//       if (this.state.y > 1) {
+//         this.setState({
+//           ...this.state,
+//           x: this.state.y - 1,
+//           count: this.state.count + 1,
+//         });
+//       } else {
+//         this.setState({
+//           ...this.state,
+//           directionMessage: "You can't go up",
+//         });
+//       }
+//     } 
+    
+//     else if (id === "down") {
+//       if (this.state.y < 3) {
+//         this.setState({
+//           ...this.state,
+//           x: this.state.y + 1,
+//           count: this.state.count + 1,
+//         });
+//       } else {
+//         this.setState({
+//           ...this.state,
+//           directionMessage: "You can't go down",
+//         });
+//       }
+//     }
