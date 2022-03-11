d89ff2b780f752f4ab4bc2fed6e12befc04f7305
Rebecca-Stone <primstone16@gmail.com>
2022-03-11 00:10:43 -0600

    was able to figure out coordinate and count logic

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,80 +1,145 @@
 import React from "react";
-import axios from 'axios'
+import axios from "axios";
 
-const URL = 'http://localhost:9000/api/result';
+const URL = "http://localhost:9000/api/result";
 
-const x = 2;
-const y = 2;
+// let x = 2;
+// let y = 2;
+// let allSteps = 0;
 
 const initialState = {
-  coordinates: {
-    "x": x, 
-    "y": y,
-  },
-  "steps": 3, 
+  x: 2,
+  y: 2,
+
+  count: 0,
   form: {
-    "email": ''
-  }, 
-  submitMessage: '',
-  directionMessage: '',
-}
+    email: "",
+  },
+  submitMessage: "",
+  directionMessage: "",
+};
 
 export default class AppClass extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = initialState
+    super(props);
+    this.state = initialState;
   }
 
-  componentDidMount(){
-    console.log('this is state when mounting', this.state);
+  componentDidMount() {
+    // console.log("this is state when mounting", this.state);
+  }
+
+  componentDidUpdate() {
+    // console.log("this is what state is now", this.state);
   }
 
   getMessage = () => {
     const newPlayer = {
-      "x": this.state.coordinates["x"],
-      "y": this.state.coordinates["y"],
-      "steps": this.state["steps"],
-      "email": this.state.form["email"]
-    }
-    console.log('this is the newPlayer',newPlayer)
-    axios.post(URL, newPlayer)
-     .then(res => {
-       console.log('this is the posting res', res)
-      this.setState({
-        ...this.state, 
-        submitMessage: [...this.state.submitMessage, res.data.message]
+      x: this.state.x,
+      y: this.state.y,
+      steps: this.state.count,
+      email: this.state.form.email,
+    };
+    // console.log("this is the newPlayer", newPlayer);
+    axios
+      .post(URL, newPlayer)
+      .then((res) => {
+        // console.log("this is the posting res", res);
+        this.setState({
+          ...this.state,
+          submitMessage: [...this.state.submitMessage, res.data.message],
+        });
       })
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  onSubmit = evt => {
-    evt.preventDefault()
-    this.getMessage()
-  }
+  onSubmit = (evt) => {
+    evt.preventDefault();
+    this.getMessage();
+  };
 
   changeInput = (key, value) => {
     this.setState({
       ...this.state,
-      form: {...this.state.form, [key]: value } 
-    })
-  }
+      form: { ...this.state.form, [key]: value },
+    });
+  };
 
-  onChange = evt => {
-    const { value, id } = evt.target
+  onChange = (evt) => {
+    const { value, id } = evt.target;
     this.changeInput(id, value);
-  }
+  };
+
+  onClick = (id) => {
+    if (id === "left") {
+      if (this.state.x > 1) {
+        this.setState({
+          ...this.state,
+          x: this.state.x - 1,
+          count: this.state.count + 1,
+        });
+      } else {
+        this.setState({
+          ...this.state,
+          directionMessage: "You can't go left",
+        });
+      }
+    } else if (id === "right") {
+      if (this.state.x < 3) {
+        this.setState({
+          ...this.state,
+          x: this.state.x + 1,
+          count: this.state.count + 1,
+        });
+      } else {
+        this.setState({
+          ...this.state,
+          directionMessage: "You can't go right",
+        });
+      }
+    } else if (id === "up") {
+      if (this.state.y > 1) {
+        this.setState({
+          ...this.state,
+          x: this.state.y - 1,
+          count: this.state.count + 1 
+        });
+      } else {
+        this.setState({
+          ...this.state,
+          directionMessage: "You can't go up",
+        });
+      }
+    } else if (id === "down") {
+      if (this.state.y < 3) {
+        this.setState({
+          ...this.state,
+          x: this.state.y + 1,
+          count: this.state.count + 1 
+        });
+      } else {
+        this.setState({
+          ...this.state,
+          directionMessage: "You can't go down",
+        });
+      }
+    }
+    console.log("x:", this.state.x, "y:", this.state.y);
+    console.log("steps:", this.state.count);
+  };
 
   render() {
-    const { coordinates, steps, submitMessage, directionMessage } = this.state;
+    const { x, y, count, submitMessage, directionMessage } = this.state;
     const { className } = this.props;
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates ({coordinates["x"]}, {coordinates["y"]})</h3>
-          <h3 id="steps">You moved {steps} times</h3>
+          <h3 id="coordinates">
+            Coordinates ({x}, {y})
+          </h3>
+          <h3 id="steps">You moved {count} times</h3>
         </div>
         <div id="grid">
           <div className="square"></div>
@@ -88,18 +153,34 @@ export default class AppClass extends React.Component {
           <div className="square"></div>
         </div>
         <div className="info">
-          <h3 id="message">{submitMessage}{directionMessage}</h3>
+          <h3 id="message">
+            {submitMessage}
+            {directionMessage}
+          </h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
+          <button id="left" onClick={() => this.onClick("left")}>
+            LEFT
+          </button>
+          <button id="up" onClick={() => this.onClick("up")}>
+            UP
+          </button>
+          <button id="right" onClick={() => this.onClick("right")}>
+            RIGHT
+          </button>
+          <button id="down" onClick={() => this.onClick("down")}>
+            DOWN
+          </button>
           <button id="reset">reset</button>
         </div>
         <form onSubmit={this.onSubmit}>
-          <input onChange={this.onChange} id="email" type="email" placeholder="type email"></input>
-          <input id="submit" type="submit" ></input>
+          <input
+            onChange={this.onChange}
+            id="email"
+            type="email"
+            placeholder="type email"
+          ></input>
+          <input id="submit" type="submit"></input>
         </form>
       </div>
     );
@@ -111,41 +192,7 @@ export default class AppClass extends React.Component {
 // (1, 2)(2, 2)(3, 2)
 // (1, 3)(2, 3)(3, 3);
 
-// moving up --y 
+// moving up --y
 // moving down ++y
 // moving right ++x
 //moving left --x
-
-// onClick = id => {
-  // if(id == "left"){
-    // this.setState({
-      // ...this.state,
-    // coordinates["x"]: --x
-    // })else if(id == "right"){
-      // this.setState({
-        // ...this.state,
-        // coordinates["x"]: ++x
-      // })else if(id == "up"){
-        // this.setState({
-          // ...this.state,
-          // coordinates["y"]: --y
-        // })else if(id == "down"){
-          // this.setState({
-            // ...this.state,
-            //  coordinates["y"]: ++y
-          // }) 
-        // }
-      // } 
-    // }
-  // }
-// }
-
-// onClick={evt => this.onClick(id)} 
-
-{/* 
-<button id="left">LEFT</button>
-<button id="up">UP</button>
-<button id="right">RIGHT</button>
-<button id="down">DOWN</button>
-<button id="reset">reset</button> 
-*/}
