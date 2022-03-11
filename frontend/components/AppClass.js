b70519150894382113ff19505d1e73542fae0058
Rebecca-Stone <primstone16@gmail.com>
2022-03-10 20:59:56 -0600

    working on making some of the class features more dynamic

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,13 +1,63 @@
-import React from 'react'
+import React from "react";
+import axios from 'axios'
+
+const URL = 'http://localhost:9000/api/result';
+
+const initialState = {
+  coordinates: {
+    x: 1, 
+    y: 2,
+  },
+  steps: 3, 
+  email: "lady@gaga.com", 
+  submitMessage: '',
+  directionMessage: '',
+}
 
 export default class AppClass extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = initialState
+  }
+
+  componentDidMount(){
+    this.getMessage()
+  }
+
+  getMessage = () => {
+    const newPlayer = {
+      "x": this.state.coordinates.x,
+      "y": this.state.coordinates.y,
+      "steps": this.state.steps,
+      "email": this.state.email
+    }
+    axios.post(URL, newPlayer)
+     .then(res => {
+      this.setState({
+        ...this.state, 
+        submitMessage: [...this.state.submitMessage, res.message]
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  changeEmail = (key, value) => {
+    this.setState({
+      ...this.state,
+      "email": { ...this.state["email"], [key]: value }
+    })
+  }
+
   render() {
-    const { className } = this.props
+    const { coordinates, steps, submitMessage, directionMessage } = this.state;
+    const { className } = this.props;
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates ({coordinates.x}, {coordinates.y})</h3>
+          <h3 id="steps">You moved {steps} times</h3>
         </div>
         <div id="grid">
           <div className="square"></div>
@@ -21,7 +71,7 @@ export default class AppClass extends React.Component {
           <div className="square"></div>
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{submitMessage}{directionMessage}</h3>
         </div>
         <div id="keypad">
           <button id="left">LEFT</button>
@@ -35,6 +85,6 @@ export default class AppClass extends React.Component {
           <input id="submit" type="submit"></input>
         </form>
       </div>
-    )
+    );
   }
 }
